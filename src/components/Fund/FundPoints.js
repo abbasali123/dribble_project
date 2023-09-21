dcf34d1fe6447f8a6314a63ad1941a9b3f8f7f90
Muhammad saad <muhammad.saad@tkxel.com>
2023-09-21 14:59:04 +0500

    add components

---
+++ src/components/Fund/FundPoints.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function FundPoints({ description }) {
+  return (
+    <div style={{ display:"flex", flexDirection:"column", alignItems:"flex-start"}}>
+      <div><img src='circle.png' height="55px"/></div>
+      <p style={{ margin:"20px 0 40px 5px" }} dangerouslySetInnerHTML={{ __html: description }}></p>
+    </div>
+  );
+}
+
+export default FundPoints;
